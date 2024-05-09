88d102d2d09a3d3de3bf6c748c22e356a52d4536
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-05-09 11:02:00 -0300

    refactoring buttons

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -6,8 +6,8 @@ import { Button } from "@/components/ui/button";
 export const Header = () => {
   return (
     <header className="border-b-2 border-gray-300 py-2">
-      <div className="max-w-4xl mx-auto flex justify-between items-center">
-        <div className="flex items-center">
+      <div className="max-w-4xl mx-auto flex justify-center items-center">
+        <div className="flex items-center justify-between w-full">
           <div>
             <Image
               src="/logoDisfrutaParaguay.png"
@@ -16,25 +16,24 @@ export const Header = () => {
               alt="Logo Disfruta Paraguay"
             />
           </div>
-
           <nav>
             <ul className="flex justify-center items-center space-x-4">
-              <li>
+              <li className="pl-2 pr-2">
                 <Link href="/">
-                  <p className="text-gray-600 hover:text-gray-900">Home</p>
+                  <p>Home</p>
                 </Link>
               </li>
-              <li>
+              <li className="pl-2 pr-2">
                 <Link href="/about">
-                  <p className="text-gray-600 hover:text-gray-900">About</p>
+                  <p>About</p>
                 </Link>
               </li>
-              <li>
+              <li className="pl-2 pr-2">
                 <Link href="/sign-in">
                   <Button>Sign In</Button>
                 </Link>
               </li>
-              <li>
+              <li className="pl-2 pr-2">
                 <ModeToggle />
               </li>
             </ul>
