33961855184004cf6b7888283afa34da944190e9
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-05-27 17:16:51 -0300

    correcting prettier

--- src/app/(restricted-area)/users/page.tsx
+++ src/app/(restricted-area)/users/page.tsx
@@ -1,11 +1,8 @@
-import { ArrowRight, Search, X } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { UserTableFilters } from "@/app/components/UserTableFilters";
+import { UserTableRow } from "@/app/components/UserTableRow";
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
@@ -18,10 +15,7 @@ export default function Users() {
         <h1 className="text-3xl font-bold tracking-tight">Users</h1>
       </div>
       <div className="space-y-2.5 ">
-        <form className="flex items-center gap-2 pb-5">
-          <span className="text-sm font-semibold">Filtros</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
+        <UserTableFilters />
         <div className="rounded-md border">
           <Table>
             <TableHeader>
@@ -37,45 +31,9 @@ export default function Users() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Button variant="outline" size="sm">
-                    <Search className="h-3 w-3" />
-                    <span className="sr-only">Detalhes do pedido</span>
-                  </Button>
-                </TableCell>
-                <TableCell className="font-mono text-xs font-medium">
-                  qweqwewqeqw
-                </TableCell>
-                <TableCell className="text-muted-foreground">
-                  há 15 minutos
-                </TableCell>
-                <TableCell>
-                  <div className="flex items-center gap-2">
-                    <span className="h-2 w-2 rounded-full bg-slate-400" />
-                    <span className="font-medium text-muted-foreground">
-                      Pendente
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell className="font-medium">
-                  Rafael Costa Palma
-                </TableCell>
-                <TableCell className="font-medium"> R$ 149.90</TableCell>
-                <TableCell>
-                  <Button variant="outline" size="sm">
-                    <ArrowRight className="mr-2 h-3 w-3" />
-                    Aprovar
-                  </Button>
-                </TableCell>
-                <TableCell>
-                  <Button variant="ghost" size="sm">
-                    <X className="mr-2 h-3 w-3" />
-                    Cancelar
-                  </Button>
-                </TableCell>
-                <TableCell></TableCell>
-              </TableRow>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return <UserTableRow key={i} />;
+              })}
             </TableBody>
           </Table>
         </div>
---
+++ src/app/components/Pagination/index.tsx
@@ -0,0 +1,37 @@
+import { ChevronsLeft } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+interface PaginationProps {
+  pageIndex: number;
+  totalCount: number;
+  perPage: number;
+}
+
+export function Pagination({
+  pageIndex,
+  totalCount,
+  perPage,
+}: PaginationProps) {
+  const pages = Math.ceil(totalCount / perPage) || 1;
+
+  return (
+    <div className="flex items-center justify-between">
+      <span className="text-sm text-muted-foreground">
+        Total de {totalCount} item(s)
+      </span>
+
+      <div className="flex items-center gap-6 lg:gap-8">
+        <div className="text-sm font-medium">
+          Página {pageIndex + 1} de {pages}
+        </div>
+        <div className="flex items-center gap-2">
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronsLeft className="h-4 w-4" />
+            <span className="sr-only">Primeira página</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
