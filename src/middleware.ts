7d9c57d0f3db6fb514d985efd6de06b21b860310
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-06-16 11:00:32 -0300

    adjusting auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,19 @@
 import type { NextRequest } from "next/server";
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import axios from "axios";
-// import jwt from "jsonwebtoken";
 
-// interface Token {
-//   name: string;
-//   value: string;
-// }
+interface Token {
+  name: string;
+  value: string;
+}
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("@web-disfrutaparaguay");
+  const token: Token | undefined = request.cookies.get("@web-disfrutaparaguay");
 
   if (token) {
-    console.log("token.value:", token.value);
-
     try {
-      const response = await axios.post(
-        "https://api-disfruta-paraguay.onrender.com/me",
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/me`, // TODO: Ajustar esse .env para env.js
         null,
         {
           headers: {
@@ -24,13 +21,9 @@ export async function middleware(request: NextRequest) {
           },
         },
       );
-      console.log("Response:", response.data);
-      console.log("o token é valido");
-      //   return NextResponse.next();
+      return NextResponse.next();
     } catch (err) {
-      console.log("o token é invalido", err);
-      //   return NextResponse.redirect(new URL("/sign-in", request.url));
-      //   return NextResponse.redirect(`/sign-in`);
+      return NextResponse.redirect(new URL("/sign-in", request.url));
     }
   }
 }
